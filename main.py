aa40f75e6807698bdf8264a9b2caedc40cad7b99
Moein Arabi <moein.mo81@gmail.com>
2023-02-04 07:56:29 +0330

    Add new sounds

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from pynput import keyboard
 from threading import Thread
+from playsound import playsound
 import winsound
 
 
-def beep():
-    winsound.Beep(800, 60)
+def beep(key):
+    if sound_mode == 0:
+        winsound.Beep(800, 60)
+    elif sound_mode == 1:
+        if key == keyboard.Key.backspace:
+            playsound('sounds/backspace.wav')
+        elif key == keyboard.Key.enter:
+            playsound('sounds/enter.wav')
+        else:
+            playsound('sounds/keyboard.wav')
 
 
-def on_release(_):
-    Thread(target=beep).start()
+def on_release(key):
+    Thread(target=beep, args=(key,)).start()
 
 
-with keyboard.Listener(on_release=on_release) as listener:
-    listener.join()
+listener = keyboard.Listener(on_release=on_release)
+listener.start()
+sound_mode = 0
+while (True):
+    try:
+        sound_mode = int(input())
+    except ValueError as e:
+        print(e)
